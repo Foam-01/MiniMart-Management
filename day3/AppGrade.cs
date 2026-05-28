940fa24c25751fe461460b489f1af1e978015228
Foam <ddotdka4@gmail.com>
2026-05-28 09:04:16 +0700

    Refine grading logic in AppGrade class and update assembly information

--- day3/AppGrade.cs
+++ day3/AppGrade.cs
@@ -15,18 +15,30 @@ namespace TestConsole.day3
            if (score < 50)
             {
                 Console.WriteLine("Your grade is F");
-            } else if (score <= 60)
+            } else if (score >= 50 && score < 55)
             {
                 Console.WriteLine("Your grade is D");
-            } else if (score <= 70)
+            } else if (score >= 55 && score < 60)
+            {
+                Console.WriteLine("Your grade is D+");
+            } else if (score >= 60 && score < 65)
             {
                 Console.WriteLine("Your grade is C");
-            } else if (score <= 80)
+            } else if (score >= 65 && score < 70)
+            {
+                Console.WriteLine("Your grade is C+");
+            } else if (score >= 70 && score < 75)
             {
                 Console.WriteLine("Your grade is B");
-            } else
+            } else if (score >= 75 && score < 80)
+            {
+                Console.WriteLine("Your grade is B+");
+            } else if (score >= 80 && score <= 100)
             {
                 Console.WriteLine("Your grade is A");
+            } else 
+            {
+                Console.WriteLine("Invalid score");
             }
         }
     }
