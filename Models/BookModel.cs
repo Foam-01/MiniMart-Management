a272575c9cfbace225d18b87cfa2f94d7399f502
Foam-01 <ddotdka4@gmail.com>
2026-07-21 11:35:49 +0700

    chore: initialize node_modules directory for frontend application

--- Models/BookModel.cs
+++ Models/BookModel.cs
@@ -4,8 +4,8 @@ namespace CourseAPI.Models
     public class BookModel
     {
         public int id { get; set; }
-        public string isbn { get; set; }
-        public string name { get; set; }
+        public string? isbn { get; set; }
+        public string? name { get; set; }
         public int price { get; set; }
     }
 }
